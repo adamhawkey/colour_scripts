5cf26b55161c47d2028165842a5715e06fc2a8c7
Adam Hawkey <atomahoke@gmail.com>
2020-12-06 19:19:11 -0800

    minor formatting PQtest_01

--- PQtest_01.py
+++ PQtest_01.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # Luminance calculation of Perceptual Quantizer (PQ) EOTF
 # From the SMPTE / Dolby presentation
 # "A Perceptual EOTF for Extended Dynamic Range Imagery"
 
-N=float(input("Enter a value: "))
+N = float(input("Enter a float value: "))
 
-m1=(2610/4096)/4
-m2=(2523/4096)*128
-c1=3424/4096
-c2=(2413/4096)*32
-c3=(2392/4096)*32
-C=10000
+m1 = (2610/4096)/4
+m2 = (2523/4096)*128
+c1 = 3424/4096
+c2 = (2413/4096)*32
+c3 = (2392/4096)*32
+C = 10000
 
 print(N)
 
